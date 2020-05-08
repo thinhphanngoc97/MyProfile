dbb3e3585a1d2e70899d0168d915a2faa5a346e6
thinhphanngoc97 <thinhphanngoc97@gmail.com>
2020-05-08 10:26:23 +0700

    done about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,56 @@ class About extends Component {
             <div className="container">
                 <div id="about">
                     <div className="row">
-                        <div className="col-md-4 col-sm-12">
+                        <div className="col-lg-4 col-md-12 col-sm-12">
                             <img className="about-image" src={aboutImg} alt="Still me"/>
                             <img className="stack-about-image about-image" src={aboutImg} alt=""/>
                         </div>
-                        <div className="about-content col-md-8 col-sm-12">
+                        <div className="about-content col-lg-8 col-md-12 col-sm-12">
                             <h2>Front-end Developer</h2>
+                            <p>I am <b>Thinh Phan Ngoc</b>. I am a simple, creative, enthusiastic and fun-loving person.</p>
+                            <p>My objective is always constantly improving programming skills and understanding of technologies to become a professional front-end developer.</p>
+                            <div className="row personal-info">
+                                <div className="col-md-6 col-sm-12">
+                                    <ul>
+                                        <li>
+                                            <span className="title">Name : </span>
+                                            <span className="value">Thinh Phan Ngoc</span>
+                                        </li>
+                                        <li>
+                                            <span className="title">Age : </span>
+                                            <span className="value">24 Years</span>
+                                        </li>
+                                        <li>
+                                            <span className="title">Address : </span>
+                                            <span className="value">Go Vap District, Ho Chi Minh City</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <ul>
+                                        <li>
+                                            <span className="title">Phone : </span>
+                                            <span className="value">(+84) 98 869 03 45</span>
+                                        </li>
+                                        <li>
+                                            <span className="title">Email : </span>
+                                            <span className="value">thinhphanngoc97@gmail.com</span>
+                                        </li>
+                                        <li>
+                                            <span className="title">Hobby : </span>
+                                            <span  className="value">Movie, Music, Travel</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <a 
+                                className="btn btn-download-cv" 
+                                href="https://www.topcv.vn/xem-cv/0ff40b88b7c1a22667ffa3dcbf60942d" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                            >
+                                Download CV
+                            </a>
                         </div>
                     </div>
                 </div>
